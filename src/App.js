a384829b0354732a33ea9038d6adda0893711ee3
Girdhar2 <vanshikajtwg839@gmail.com>
2022-07-14 03:44:18 +0530

    deploy crypto-website

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import { makeStyles } from "@material-ui/core";
+import Homepage from "./Pages/HomePage";
+import "./App.css";
+import { BrowserRouter, Route } from "react-router-dom";
+import CoinPage from "./Pages/CoinPage";
+import Header from "./components/Header";
+
+const useStyles = makeStyles(() => ({
+  App: {
+    backgroundColor: "#14161a",
+    color: "white",
+    minHeight: "100vh",
+  },
+}));
+
+function App() {
+  const classes = useStyles();
+
+  return (
+    <BrowserRouter>
+      <div className={classes.App}>
+        <Header />
+        <Route path="/coins/:id" component={CoinPage} exact />
+          <Route path="/Crypto-Hunter" component={Homepage} exact/>
+          <Route path="/" component={Homepage} exact/>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
